d196b753c48e3dec2f1b0f1f4a2333d8cea37360
Pierre-Olivier Bedard <pobed2@gmail.com>
2015-07-19 14:00:58 -0400

    Added separate menu for map.

--- public/modules/stores/config/stores.client.config.js
+++ public/modules/stores/config/stores.client.config.js
@@ -5,7 +5,7 @@ angular.module('stores').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Stores', 'stores', 'dropdown', '/stores(/create)?');
-		Menus.addSubMenuItem('topbar', 'stores', 'Map', 'map');
+		Menus.addMenuItem('topbar', 'Map', 'map', 'map');
 		Menus.addSubMenuItem('topbar', 'stores', 'List Stores', 'stores');
 		Menus.addSubMenuItem('topbar', 'stores', 'New Store', 'stores/create');
 	}
