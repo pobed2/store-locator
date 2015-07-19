f6c1808bca921792d44683eacf1307f9bc2df8f4
Pierre-Olivier Bedard <pobed2@gmail.com>
2015-07-19 13:52:40 -0400

    Moved map to top of "stores" menu

--- public/modules/stores/config/stores.client.config.js
+++ public/modules/stores/config/stores.client.config.js
@@ -5,8 +5,8 @@ angular.module('stores').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Stores', 'stores', 'dropdown', '/stores(/create)?');
+		Menus.addSubMenuItem('topbar', 'stores', 'Map', 'map');
 		Menus.addSubMenuItem('topbar', 'stores', 'List Stores', 'stores');
 		Menus.addSubMenuItem('topbar', 'stores', 'New Store', 'stores/create');
-		Menus.addSubMenuItem('topbar', 'stores', 'Map', 'map');
 	}
 ]);
